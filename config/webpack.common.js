2999c889942c592ca097933396baeb765fd0bd3c
webdingo <57765270+webdingo@users.noreply.github.com>
2020-10-05 15:20:06 +0100

    Cleaned up default boilerplate

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,7 +1,7 @@
-const paths = require('./paths')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const paths = require("./paths");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   /**
@@ -9,7 +9,7 @@ module.exports = {
    *
    * The first place Webpack looks to start building the bundle.
    */
-  entry: [paths.src + '/index.js'],
+  entry: [paths.src + "/index.js"],
 
   /**
    * Output
@@ -18,8 +18,8 @@ module.exports = {
    */
   output: {
     path: paths.build,
-    filename: '[name].bundle.js',
-    publicPath: '/',
+    filename: "[name].bundle.js",
+    publicPath: "/",
   },
 
   /**
@@ -44,9 +44,9 @@ module.exports = {
       patterns: [
         {
           from: paths.static,
-          to: 'assets',
+          to: "assets",
           globOptions: {
-            ignore: ['*.DS_Store'],
+            ignore: ["*.DS_Store"],
           },
         },
       ],
@@ -58,10 +58,10 @@ module.exports = {
      * Generates an HTML file from a template.
      */
     new HtmlWebpackPlugin({
-      title: 'Webpack Boilerplate',
-      favicon: paths.static + '/favicon.png',
-      template: paths.src + '/template.html', // template file
-      filename: 'index.html', // output file
+      title: "Shopify Dev Theme",
+      favicon: paths.static + "/favicon.png",
+      template: paths.src + "/template.html", // template file
+      filename: "index.html", // output file
     }),
   ],
 
@@ -80,7 +80,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"],
       },
 
       /**
@@ -91,10 +91,13 @@ module.exports = {
       {
         test: /\.(scss|css)$/,
         use: [
-          'style-loader',
-          { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
-          { loader: 'postcss-loader', options: { sourceMap: true } },
-          { loader: 'sass-loader', options: { sourceMap: true } },
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { sourceMap: true, importLoaders: 1 },
+          },
+          { loader: "postcss-loader", options: { sourceMap: true } },
+          { loader: "sass-loader", options: { sourceMap: true } },
         ],
       },
 
@@ -105,10 +108,10 @@ module.exports = {
        */
       {
         test: /\.(?:ico|gif|png|jpg|jpeg|webp|svg)$/i,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[path][name].[ext]',
-          context: 'src', // prevent display of src/ in filename
+          name: "[path][name].[ext]",
+          context: "src", // prevent display of src/ in filename
         },
       },
 
@@ -119,13 +122,13 @@ module.exports = {
        */
       {
         test: /\.(woff(2)?|eot|ttf|otf|)$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 8192,
-          name: '[path][name].[ext]',
-          context: 'src', // prevent display of src/ in filename
+          name: "[path][name].[ext]",
+          context: "src", // prevent display of src/ in filename
         },
       },
     ],
   },
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,2 @@
-// Test import of a JavaScript function, an SVG, and Sass
-import {HelloWorld} from './js/HelloWorld'
-import WebpackLogo from './images/webpack-logo.svg'
-import './styles/index.scss'
-
-// Create SVG logo node
-const logo = document.createElement('img')
-logo.src = WebpackLogo
-
-// Create heading node
-const heading = document.createElement('h1')
-heading.textContent = HelloWorld()
-
-// Append SVG and heading nodes to the DOM
-const app = document.querySelector('#root')
-app.append(logo, heading)
+// Main imports
+import "./styles/index.scss";
